4d2bd79adafd89a72bbd13e37fc98dabd210e68b
davecoetest1 <davidcoe@Davids-MacBook-Pro.local>
2016-05-22 20:23:15 +0100

    Correct test

--- app/app.component.spec.ts
+++ app/app.component.spec.ts
@@ -25,4 +25,15 @@ describe('AppComponent', () => {
         LoggerService,
         ROUTER_FAKE_PROVIDERS
     ]);
+
+    it('should have brand WatchList', async(inject([TestComponentBuilder],
+        (tsb: TestComponentBuilder) => {
+            tsb.createAsync(TestComponent).then((fixture) => {
+                fixture.detectChanges();
+                let compiled = fixture.debugElement.nativeElement;
+                expect(compiled).toBeDefined();
+                expect(compiled.querySelector('a.navbar-brand'))
+                    .toHaveText('WatchList');
+            });
+        })));
 });
